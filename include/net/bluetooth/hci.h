535287b7b06a3c7e0b0cf8ec7f46b51fc721b107
unifreq <flippy@sina.com>
2021-11-05 22:39:17 +0800

    Apply armbian rockchip64 patches

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -246,6 +246,13 @@ enum {
 	 * HCI after resume.
 	 */
 	HCI_QUIRK_NO_SUSPEND_NOTIFIER,
+
+	/* When this quirk is set, max_page for local extended features
+	 * is set to 1, even if controller reports higher number. Some
+	 * controllers (e.g. RTL8723CS) report more pages, but they
+	 * don't actually support features declared there.
+	 */
+	HCI_QUIRK_BROKEN_LOCAL_EXT_FTR_MAX_PAGE,
 };
 
 /* HCI device flags */
