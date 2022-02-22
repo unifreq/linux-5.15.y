32a4d7d895f9052137c4a305750a579baf793b66
unifreq <flippy@sina.com>
2022-02-22 13:11:20 +0800

    drivers: meson: vdec: add VP9 support to GXM

--- drivers/staging/media/meson/vdec/vdec_platform.c
+++ drivers/staging/media/meson/vdec/vdec_platform.c
@@ -116,6 +116,18 @@ static const struct amvdec_format vdec_formats_gxl[] = {
 
 static const struct amvdec_format vdec_formats_gxm[] = {
 	{
+		.pixfmt = V4L2_PIX_FMT_VP9,
+		.min_buffers = 16,
+		.max_buffers = 24,
+		.max_width = 3840,
+		.max_height = 2160,
+		.vdec_ops = &vdec_hevc_ops,
+		.codec_ops = &codec_vp9_ops,
+		.firmware_path = "meson/vdec/gxl_vp9.bin",
+		.pixfmts_cap = { V4L2_PIX_FMT_NV12M, 0 },
+		.flags = V4L2_FMT_FLAG_COMPRESSED |
+			 V4L2_FMT_FLAG_DYN_RESOLUTION,
+	}, {
 		.pixfmt = V4L2_PIX_FMT_HEVC,
 		.min_buffers = 4,
 		.max_buffers = 24,
